c7a2752a7ce4a3e9f25f0442042020b66f7d137e
Nurmat Junusov <n.zhunusov@nambasoft.com>
2025-08-21 14:40:44 +0600

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Nurmat Junusov on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
